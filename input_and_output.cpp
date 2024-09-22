f5e0985f0e8507f2bbf1a56385adffd89ca207f8
Catalia20 <imaneettis2@gmail.com>
2024-09-23 00:04:05 +0100

    Create input_and_output.cpp

---
+++ input_and_output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#includ <cstdio>
+#includ <vector>
+#includ <iostream>
+#includ <algorithm>
+using namespace std;
+
+
+ int main() {
+     int a,b,c;
+     cin >> a >> b >> c;
+     int result = a + b c;
+     cout << result;
+     return 0;
+ }
